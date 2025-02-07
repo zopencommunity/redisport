0df879b179c032e7ce4250a1047033618a5a6278
CommunistPotato1231 <khuang2024@gmail.com>
2025-02-07 13:28:26 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/redisport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/redisport/actions/workflows/bump.yml)
+
+# redis
+
+Redis is an in-memory database that persists on disk.
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/redisport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/redisport.git'), string(name: 'PORT_DESCRIPTION', value: 'Redis is an in-memory database that persists on disk.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
